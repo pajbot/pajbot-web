4119f83296e7d5b010c64518400171c25eb558d2
Rasmus Karlsson <pajlada@bithack.se>
2016-10-03 18:01:49 +0000

    removed port shit from websocket

--- src/components/stream/clr.vue
+++ src/components/stream/clr.vue
@@ -7,7 +7,7 @@
 <script>
 import ReconnectableWebSocket from 'reconnectable-websocket'
 
-let socket = new ReconnectableWebSocket('wss://' + window.location.hostname + ':2355/ws/clr')
+let socket = new ReconnectableWebSocket('wss://' + window.location.hostname + '/ws/clr')
 
 let innerEmotes = []
 
